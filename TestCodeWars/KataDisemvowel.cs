a35ba70ecb4941da018e84e07f2a7b8eefde4faf
Rudi Folla <rudi.folla@gmail.com>
2023-03-28 23:18:57 +0200

    Mask kata and Disemvowel Kata
    Signed-off-by: Rudi Folla <rudi.folla@gmail.com>

---
+++ TestCodeWars/KataDisemvowel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestCodeWars
+{
+    public class KataDisemvowel
+    {
+        //https://towardsdatascience.com/regular-expressions-clearly-explained-with-examples-822d76b037b4
+        public static string Disemvowel(string str)
+        {
+            string pattern = "[a|A|e|E|i|I|o|O|u|U]{1,1}";
+            Regex regex = new(pattern);
+            MatchCollection matchCollection = regex.Matches(str);
+            List<string> done = new();
+            foreach (Match match in matchCollection)
+            {
+                if (done.Contains(match.Value))
+                    continue;
+                else
+                {
+                    str = str.Replace(match.Value, "");
+                    done.Add(match.Value);
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ TestCodeWars/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestCodeWars;
+
+var a = "Ths wbst s fr lsrs LL!" == KataDisemvowel.Disemvowel("?&^n OJ HKL CC A;Oof!VYUVO[Tbr");
+var b = "1" == KataMask.Maskify("1");
+var c = "#1111" == KataMask.Maskify("11111");
+Console.WriteLine("{0} {1} {2}", a,b,c);
