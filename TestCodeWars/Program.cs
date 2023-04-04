91471e11e0c7ccb3582dad15f4184acf627ac28b
RudiFolla <folla.rudi@gmail.com>
2023-04-04 23:59:43 +0200

    Kata move zeroes in an array

--- TestCodeWars/Program.cs
+++ TestCodeWars/Program.cs
@@ -1,25 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TestCodeWars;
-
-var testarr = new string[]{
-"4873279",
-"ITS-EASY",
-"888-4567",
-"3-10-10-10",
-"888-GLOP",
-"TUT-GLOP",
-"967-11-11",
-"310-GINO",
-"F101010",
-"888-1200",
-"-4-8-7-3-2-7-9-",
-"487-3279"};
-var result = new string[]{
-"310-1010:2",
-"487-3279:4",
-"888-4567:3"};
-var a = result == KataPhoneDuplicate.FindDuplicatePhoneNumbers(testarr);
-Console.WriteLine(a);
+var a = KataArrayMoveZeroes.MoveZeroes(new int[] { 1, 0, 1, 1, 0, 1, 0, 3, 0, 1, 0, 1, 2, 0, 9, 7, 5, 0, 0, 0, 0, 0, 0, 3 });
+//var c = KataArrayMoveZeroes.MoveZeroes(new int[] { 0, 0, 0, 0, 3 });
+var b = new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 };
+Console.WriteLine( "a");
 
 
 
