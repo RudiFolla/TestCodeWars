5b5a0dab0149771be9fc4e8495679d28d3f5d156
RudiFolla <folla.rudi@gmail.com>
2023-04-11 22:48:42 +0200

    Kata Rgb to Hex

---
+++ TestCodeWars/KataRgbToHex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCodeWars
+{
+    public class KataRgbToHex
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            string result = "";
+            int[] rgbValues = { r, g, b };
+
+            for (int i = 0; i < rgbValues.Length; i++)
+            {
+                if (rgbValues[i] < 0)
+                    rgbValues[i] = 0;
+                else if (rgbValues[i] > 255)
+                    rgbValues[i] = 255;
+                var tmp = Convert.ToString(rgbValues[i], 16);
+                tmp = tmp.Length < 2 ? tmp.PadLeft(2, '0') : tmp;
+                result += tmp;
+            }
+            return result.ToUpper();
+        }
+    }
+}
