4ff618068528b4aa4f8b3ff458a0bea5fceac734
RudiFolla <folla.rudi@gmail.com>
2023-04-12 22:03:34 +0200

    Kata between extremes

---
+++ TestCodeWars/KataBetweenExtremes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCodeWars
+{
+    public class KataBetweenExtremes
+    {
+        public static int BetweenExtremes(int[] numbers)
+        {
+            int result;
+            //solution with linq
+            //result = numbers.Max() - numbers.Min();
+
+            int maxValue = numbers[0];
+            int minValue = numbers[0];
+            //solution with for
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxValue)
+                    maxValue = numbers[i];
+                if (numbers[i] < minValue)
+                    minValue = numbers[i];
+            }
+            result = maxValue - minValue;
+            return result;
+        }
+    }
+}
