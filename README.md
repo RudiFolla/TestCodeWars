82afab5b5daf4234dbf6199a77c69e22aba469e6
RudiFolla <folla.rudi@gmail.com>
2023-07-25 18:05:19 +0200

    Upload readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for learning languages through codewars challenges
--- TestCodeWars/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TestCodeWars;
-
-Console.WriteLine(KataIPV4ToInt32.ToInt32("128.32.10.1")== 2149583361);
-
-
-//for regex https://www.rexegg.com/regex-quickstart.html
